2e4fc92ba4a8e8b8a5c6105704f045c78da74acb
Charlie Rice <charles.e.riceiii@gmail.com>
2023-02-04 10:48:25 -0500

    debugging based on repo in ghub

--- routes.py
+++ routes.py
@@ -1,15 +1,55 @@
-app = create_app()
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    flash, 
+    url_for,
+    session
+)
+
+from datetime import timedelta
+from sqlalchemy.exc import (
+    IntegrityError,
+    DataError,
+    DatabaseError, 
+    InterfaceError,
+    InvalidRequestError,
+)
+from werkzeug.routing import BuildErrror
+
+
+from flask_bcyrpt import Bcrypt, generate_password_hash, check password_hash
+
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    current_user, 
+    logout_user,
+    login_required,
+)
+
+from app import create_app, db, login_manager, bcrypt
+from models import User
+from forms import login_form, register_form
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Home Route
+app = create_app()
+
+@app.before_request
+def session_handerl():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=1)
+
 @app.route("/", methods=("GET", "POST"), strict_slashes=False)
 def index():
     return render_template("index.html", title="Home")
 
-# Login Route
+
 @app.route("/login/", methods=("GET", "POST"), strict_slashes=False )
 def login():
     form = login_form()
@@ -24,14 +64,20 @@ def login():
                 flash("Invailid Username or password!", "danger")
         except Exception as e:
             flash(e, 'danger')
+    
+    return render_template("auth.html",
+    form=form,
+    text="Login",
+    title="Login"
+    btn_action="Login"
+    )
 
-    return render_template("auth.html", form=form)
+
 
 # Register route
 @app.route("/register/", methods=("GET", "POST"), strict_slashes=False)
 def register():
     form = register_form()
-
     if form.validate_on_submit():
         try:
             email = form.email.data
@@ -48,10 +94,31 @@ def register():
             db.session.commit()
             flash(f"Account Successfully created", "success")
             return redirect(url_for("login"))
-        except Exception as E:
-            flash(e, "danger")
-
-    return render_template("auth.html", form=form)
+        
+        except InvalidRequestError:
+            db.session.rollback()
+            flash(f"Something Went wrong!", "danger")
+        except IntegrityError:
+            db.session.rollback()
+            flash(f"User already exists!", "warning")
+        except DataError:
+            db.session.rollback()
+            flash(f"Invalid Entry", "danger")
+        except InterfaceError:
+            db.session.rollback()
+            flash(f"Error connecting to the database", "danger")
+        except DatabaseError:
+            db.session.rollback()
+            flash(f"Error connecting to the database", "danger")
+        except BuildError:
+            db.session.rollback()
+            flash(f"An error occured", "danger")
+    return render_template("auth.html", 
+    form=form,
+    text="Create account",
+    title="Register",
+    btn_action="Register account"
+    )
 
 @app.route("/logout")
 @login_required
