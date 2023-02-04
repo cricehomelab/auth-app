da8660c61c90075ad4f0ca10739ff89fee6400c0
Charlie Rice <charles.e.riceiii@gmail.com>
2023-02-04 10:03:37 -0500

    debuging this to see if we can run this.

--- app.py
+++ app.py
@@ -29,4 +29,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-    
+    login_manager.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
--- routes.py
+++ routes.py
@@ -14,6 +14,17 @@ def index():
 def login():
     form = login_form()
 
+    if form.validate_on_submit():
+        try:
+            user = User.query.filter_by(email.form.email.data).first()
+            if check_password_hash(user.pwd, form.pwd.data):
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                flash("Invailid Username or password!", "danger")
+        except Exception as e:
+            flash(e, 'danger')
+
     return render_template("auth.html", form=form)
 
 # Register route
@@ -21,7 +32,33 @@ def login():
 def register():
     form = register_form()
 
+    if form.validate_on_submit():
+        try:
+            email = form.email.data
+            pwd = form.pwd.data
+            username = form.username.data
+
+            newuser = User(
+                username=username,
+                email=email,
+                pwd=bcrypt.generate_password_hash(pwd),
+            )
+
+            db.session.add(newyuser)
+            db.session.commit()
+            flash(f"Account Successfully created", "success")
+            return redirect(url_for("login"))
+        except Exception as E:
+            flash(e, "danger")
+
     return render_template("auth.html", form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
