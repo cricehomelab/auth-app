4fb4e56970aacd1a6eb66d0d76f08d187fec8ce2
Charlie Rice <charles.e.riceiii@gmail.com>
2023-02-25 15:25:00 -0500

    commenting db model.

--- models.py
+++ models.py
@@ -5,14 +5,15 @@ class User(UserMixin, db.Model):
     __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(140), unique=False, nullable=False)
-    last_name = db.Column(db.String(140), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    pwd = db.Column(db.String(300), nullable=False, unique=True)
-    password_date = db.Column(db.Integer, nullable=False, unique=False) # Unix time for date password was last set.
-    require_password_set = db.Column(db.Integer, nullable=False, unique=False) # require password reset on next login. 
-    is_admin = db.Column(db.Integer, nullable=False, unique=False) # determines if user is an admin or not.
-    is_active = db.Column(db.Integer, nullable=False, unique=False) # determines if user is able to login.
+    first_name = db.Column(db.String(140), unique=False, nullable=False)       # First name of user.
+    last_name = db.Column(db.String(140), unique=False, nullable=False)        # Last name of user.
+    email = db.Column(db.String(120), unique=True, nullable=False)             # Email address of user.
+    pwd = db.Column(db.String(300), nullable=False, unique=True)               # Hashed user password.
+    password_date = db.Column(db.Integer, nullable=False, unique=False)        # Unix time for date password was last set.
+    last_login = db.Column(db.Integer, nullable=False, unique=False)           # Last time in Unix time that user logged in.
+    require_password_set = db.Column(db.Integer, nullable=False, unique=False) # require password reset on next login.
+    is_admin = db.Column(db.Integer, nullable=False, unique=False)             # determines if user is an admin or not.
+    is_active = db.Column(db.Integer, nullable=False, unique=False)            # determines if user is able to login.
 
     def __repr__(self):
         return '<User %r>' %self.username
