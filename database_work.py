26f69f650a2177b0ec4113367f9e994541eced7e
Charlie Rice <charles.e.riceiii@gmail.com>
2023-03-20 18:11:33 -0400

    more work on this branch debating refactoring this

--- database_work.py
+++ database_work.py
@@ -1,12 +1,15 @@
 from sqlalchemy import update, select
 
+
 # TODO: FIX THIS!!!!
-def update_login_time(user, time, db):
-    print(user.username)
-    print(time)
-    print(db)
-    stmt = (
-        select(db.user).
-        where(db.user.c.username == user.username)
-    )
-    print(f'stmt: {stmt}')
+#def update_login_time(user, time, db):
+#    print(f'user is: {user.username}')
+#    print(f' time is: {time}')
+#    print(f'db object {db}')
+#    stmt = (
+#        select(db.user).
+#        where(db.user.c.username == user.username)
+#    )
+#    print(f'stmt: {stmt}')
+#    return None
+
